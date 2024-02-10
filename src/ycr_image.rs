f6bf17f8292410e8a1655f97a0e0a0c2d7e2d1cf
Filip <filipkordic@hotmail.com>
2024-02-10 17:34:21 +0100

    fixed lines in YcbCr vectors so subsampling can be simpler

--- src/ycr_image.rs
+++ src/ycr_image.rs
@@ -2,9 +2,9 @@ use crate::BmpImage;
 
 #[derive(Debug, PartialEq)]
 pub struct YcbcrImage {
-    y_component: Vec<u8>,
-    cb_component: Vec<u8>,
-    cr_component: Vec<u8>,
+    y_component: Vec<Vec<u8>>,
+    cb_component: Vec<Vec<u8>>,
+    cr_component: Vec<Vec<u8>>,
     width_px: u32,
 }
 
@@ -15,10 +15,19 @@ impl YcbcrImage {
         let mut cr_vec = Vec::new();
         let mut i = 0;
         while i < source.rgb_vector.len() {
-            y_vec.push((0.299 * (source.rgb_vector[i + 2] as f32) + 0.587 * (source.rgb_vector[i + 1] as f32) + 0.114 * (source.rgb_vector[i] as f32)) as u8);
-            cb_vec.push((-0.1687 * (source.rgb_vector[i + 2] as f32) - 0.3313 * (source.rgb_vector[i + 1] as f32) + 0.5 * (source.rgb_vector[i] as f32) + 128 as f32) as u8);
-            cr_vec.push((0.5 * (source.rgb_vector[i + 2] as f32) - 0.4187 * (source.rgb_vector[i + 1] as f32) - 0.0813 * (source.rgb_vector[i] as f32) + 128 as f32) as u8);
-            i += 3;
+            let mut y_line_vec = Vec::new();
+            let mut cb_line_vec = Vec::new();
+            let mut cr_line_vec = Vec::new();
+            for _ in 0..source.width_px {
+            //while i as u32 % (source.width_px * 3) != 0 {
+                y_line_vec.push((0.299 * (source.rgb_vector[i + 2] as f32) + 0.587 * (source.rgb_vector[i + 1] as f32) + 0.114 * (source.rgb_vector[i] as f32)) as u8);
+                cb_line_vec.push((-0.1687 * (source.rgb_vector[i + 2] as f32) - 0.3313 * (source.rgb_vector[i + 1] as f32) + 0.5 * (source.rgb_vector[i] as f32) + 128 as f32) as u8);
+                cr_line_vec.push((0.5 * (source.rgb_vector[i + 2] as f32) - 0.4187 * (source.rgb_vector[i + 1] as f32) - 0.0813 * (source.rgb_vector[i] as f32) + 128 as f32) as u8);
+                i += 3;
+            }
+            y_vec.push(y_line_vec);
+            cb_vec.push(cb_line_vec);
+            cr_vec.push(cr_line_vec);
         }
 
         YcbcrImage {
@@ -42,9 +51,9 @@ mod tests {
             height_px: 1,
         };
         let expected = YcbcrImage {
-            y_component: vec![159, 153],
-            cb_component: vec![94, 69],
-            cr_component: vec![157, 90],
+            y_component: vec![vec![159, 153]],
+            cb_component: vec![vec![94, 69]],
+            cr_component: vec![vec![157, 90]],
             width_px: 2,
         };
 
