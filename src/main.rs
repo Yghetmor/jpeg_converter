1eee41767c1ef319cf4a08b66f00da5d41861de1
Filip <filipkordic@hotmail.com>
2024-02-10 16:04:51 +0100

    Added methods to import bitmap values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::process;
+use jpeg_converter::Args;
+use jpeg_converter::BmpImage;
+
 fn main() {
-    println!("Hello, world!");
+    let files = Args::get(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let rgb_vec = BmpImage::get_from_file(files.in_file_path);
 }
